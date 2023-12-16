e840ed30ae7734c55ea917b0ba9cc0ab43841899
Corri Naglie <csnaglie@hotmail.com>
2023-12-16 16:40:09 -0500

    added the rest of the neede js functionality and needed html elements as well as very basic css

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 const userCitySearchButton = document.getElementById('userCitySearchButton')
-const searchHistory = []
-userCitySearchButton.addEventListener('click', function () {
-
-    const userCityInput = document.getElementById('userCityInput')
-    const apiKey = '913f91851f3bfa2fdce45106f3702a7c'
-    const citySearched = userCityInput.value
-    const cityWeatherCall = `https://api.openweathermap.org/data/2.5/weather?q=${citySearched}&appid=${apiKey}&units=imperial`
-
+let searchHistory = []
+if (localStorage.history){
+    searchHistory = JSON.parse(localStorage.getItem("history"))
+}
+const historySection = document.getElementById('history')
+const apiKey = '913f91851f3bfa2fdce45106f3702a7c'
+const userCityInput = document.getElementById('userCityInput')
+function generateImg(imgData, placement) {
+    const img = document.createElement("img");
+    const iconurl = `http://openweathermap.org/img/w/${imgData}.png`
+    img.src = iconurl;
+    placement.appendChild(img)
+}
+function apiCall(cityToSearch) {
+    const cityWeatherCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=${apiKey}&units=imperial`
     fetch(cityWeatherCall)
         .then(function (response2) {
             return response2.json();
         })
         .then(function (data2) {
-            console.log(data2)
             const lat = data2.coord.lat
             const lon = data2.coord.lon
             const displayArea = document.getElementById("currentWeather")
             while (displayArea.hasChildNodes()) {
                 displayArea.removeChild(displayArea.firstChild)
             }
-            //add title for section
+            const eachDay = document.createElement("p");
+            const eachDayText = document.createTextNode(`Todays weather in ${cityToSearch}`);
+            eachDay.appendChild(eachDayText)
+            displayArea.appendChild(eachDay);
+            generateImg(data2.weather[0].icon, displayArea)
+            const windSection = document.createElement("p");
+            const windSpeed = document.createTextNode(`wind speed: ${data2.wind.speed}`);
+            windSection.appendChild(windSpeed);
+            displayArea.appendChild(windSection);
             for (const [key, value] of Object.entries(data2.main)) {
-                const currentWeather = document.createElement("p");
-                const textCurrentWeather = document.createTextNode(`${key}: ${value}`);
-                currentWeather.appendChild(textCurrentWeather);
-                displayArea.appendChild(currentWeather);
-
+                const currentWeatherSection = document.createElement("p");
+                const textCurrentWeather = document.createTextNode(`current ${key}: ${value}`);
+                currentWeatherSection.appendChild(textCurrentWeather);
+                if (key == "temp" || key == "humidity") {
+                    displayArea.appendChild(currentWeatherSection);
+                }
             }
+
             fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data)
                     const forecastDisplayArea = document.getElementById("forcastDays")
                     while (forecastDisplayArea.hasChildNodes()) {
                         forecastDisplayArea.removeChild(forecastDisplayArea.firstChild);
@@ -44,10 +59,11 @@ userCitySearchButton.addEventListener('click', function () {
                             const eachDayText = document.createTextNode(`${date[0]}`);
                             eachDay.appendChild(eachDayText);
                             forecastDisplayArea.appendChild(eachDay);
-                            const a = document.createElement("li");
-                            const b = document.createTextNode(`wind speed: ${data.list[i].wind.speed}`);
-                            a.appendChild(b);
-                            eachDay.appendChild(a);
+                            generateImg(data.list[i].weather[0].icon, eachDay)
+                            const windSection = document.createElement("li");
+                            const windSpeed = document.createTextNode(`wind speed: ${data.list[i].wind.speed}`);
+                            windSection.appendChild(windSpeed);
+                            eachDay.appendChild(windSection);
                             for (const [key, value] of Object.entries(data.list[i].main)) {
                                 const weatherForcast = document.createElement("li");
                                 const textWeatherForcast = document.createTextNode(`${key}: ${value}`);
@@ -60,6 +76,31 @@ userCitySearchButton.addEventListener('click', function () {
                     }
                 })
         })
-    searchHistory.push(userCityInput.value)
-    console.log(searchHistory)
+}
+function history() {
+    if (localStorage.history) {
+        storedHistoryArray = JSON.parse(localStorage.getItem("history"))
+        while (historySection.hasChildNodes()) {
+            historySection.removeChild(historySection.firstChild)
+        }
+        for (let i = 0; i < storedHistoryArray.length; i++) {
+            const historyButton = document.createElement("button");
+            const searchedCity = document.createTextNode(storedHistoryArray[i]);
+            historyButton.appendChild(searchedCity);
+            historyButton.addEventListener('click', function () {
+                apiCall(storedHistoryArray[i])
+            })
+            historySection.appendChild(historyButton)
+        }
+    }
+}
+history()
+userCitySearchButton.addEventListener('click', function () {
+
+    apiCall(userCityInput.value)
+    if (searchHistory.includes(userCityInput.value) === false) {
+        searchHistory.push(userCityInput.value)
+        localStorage.setItem('history', JSON.stringify(searchHistory))
+        history()
+    }
 });
